8f11e39c98ed06eae0994719dbde3f6cabd84d1d
Jenna <jenna.galka@gmail.com>
2017-02-16 15:50:30 -0500

    the beginnings of a garbage

---
+++ coopPrototype/Assets/scripts/KatamariMoveScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KatamariMoveScript : MonoBehaviour {
+
+	// get the rigidbody
+	Rigidbody rb;
+
+	// a "thrust" multiplier
+	public float thrust;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = this.GetComponent<Rigidbody> ();
+
+	}
+	
+	// FixedUpdate is for physics things
+	void FixedUpdate () {
+
+		// make it roll
+		if (Input.GetMouseButton (0)) {
+			rb.AddForce (transform.forward * thrust, ForceMode.Acceleration);
+			Debug.Log ("it's moving " + this.transform.position.x + " + " + this.transform.position.z);
+		}
+
+		// make it roll...backwards
+		// which, by the way, is nauseating
+		if (Input.GetMouseButton (1)) {
+			rb.AddForce (transform.forward * (thrust * -1), ForceMode.Acceleration);
+			Debug.Log ("it's regressing " + this.transform.position.x);
+		}
+
+		if (Input.GetKeyDown (KeyCode.A)) {
+			rb.AddForce (transform.right * (thrust * -1), ForceMode.Acceleration);
+		}
+
+		if (Input.GetKeyDown (KeyCode.D)) {
+			rb.AddForce (transform.right * (thrust), ForceMode.Acceleration);
+		}
+		
+	}
+}
---
+++ coopPrototype/Assets/scripts/PickUpScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpScript : MonoBehaviour {
+
+	GameObject katamari;
+
+	float xScale;
+	float yScale;
+	float zScale;
+
+	Vector3 scaleHalf;
+
+	void Start () {
+
+		//katamari = GameObject.Find ("Katamari");
+
+//		xScale = this.gameObject.transform.localScale.x;
+//		yScale = this.gameObject.transform.localScale.y;
+//		zScale = this.gameObject.transform.localScale.z;
+	}
+		
+//	void OnCollisionEnter (Collision other){
+//
+//		//pick it up
+//		this.transform.parent = katamari.transform;
+//
+//		//make it smaller
+//		Vector3 scaleHalf = new Vector3(xScale/2, yScale/2, zScale/2);
+//		this.transform.localScale = scaleHalf;
+//
+//		Debug.Log ("picked up by the ball");
+//
+//	}
+
+	void OnCollisionEnter (Collision other){
+
+		// pick up object it touches
+		if (!other.gameObject.name.Contains ("Plane")) {
+			other.transform.parent = this.gameObject.transform;
+
+			// make object smaller
+			xScale = other.gameObject.transform.localScale.x;
+			yScale = other.gameObject.transform.localScale.y;
+			zScale = other.gameObject.transform.localScale.z;
+
+			Vector3 scaleHalf = new Vector3 (xScale / 2, yScale / 2, zScale / 2);
+		}
+	}
+}
